d5a310727906f5b9df16f3ba3300cb9f89679526
unknown <adewumisegun92@gmail.com>
2022-05-10 07:54:22 +0100

    new update to the repo

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,33 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import React, { Fragment, useEffect, useState } from "react";
+import PreLoader from "../src/components/PreLoader";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => {
+  const [loader, setLoader] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoader(false);
+    }, 1000);
+  }, [loader]);
 
-export default MyApp
+  return (
+    <Fragment>
+      <Head>
+        <title>Fioxen - Directory & Listings React NextJs Template</title>
+        <link
+          rel='shortcut icon'
+          href='assets/images/favicon.ico'
+          type='image/png'
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Mulish:wght@400;500;600&family=Quicksand:wght@300;400;500;600;700&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
+      {loader && <PreLoader />}
+      <Component {...pageProps} />
+    </Fragment>
+  );
+};
+export default MyApp;
